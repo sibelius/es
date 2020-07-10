342c27a8b47e7d62d7abeedd9bda05c6f8a5b08c
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-07-10 08:24:25 -0300

    Updates

---
+++ precache-manifest.ce1c0c05846597eb58008c0592b4d3d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549effbe50ec83a980798d5d86887351",
+    "url": "/es/index.html"
+  },
+  {
+    "revision": "93e104f0fd246489dcf3",
+    "url": "/es/static/js/2.342ff72b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/es/static/js/2.342ff72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167e1270f2382a0894d",
+    "url": "/es/static/js/main.b2b2010d.chunk.js"
+  },
+  {
+    "revision": "67796619bccd25de1889",
+    "url": "/es/static/js/runtime-main.44540925.js"
+  }
+]);
